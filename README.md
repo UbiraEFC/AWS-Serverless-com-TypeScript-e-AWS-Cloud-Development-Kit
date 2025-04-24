73fa0b9fd21b56e5041f026476e4e0f7af57815f
Ubiratã Emiliano Fonseca Costa <ubirata.emiliano-ext@anbima.com.br>
2025-04-23 21:52:58 -0300

    feat:  firts stacks deploy and tests

--- README.md
+++ README.md
@@ -6,9 +6,24 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
-* `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `npx cdk deploy` deploy this stack to your default AWS account/region
+- `npx cdk diff` compare deployed stack with current state
+- `npx cdk synth` emits the synthesized CloudFormation template
+
+# Comandos CDK:
+
+- `cdk bootstrap` - Cria o bucket para armazenar os arquivos do CDK
+- `cdk list` - Lista as stacks que serão criadas
+- `cdk deploy --all` - Faz o deploy de todas as stacks
+- `cdk destroy` - Destrói a stack
+- `cdk destroy --all` - Destrói todas as stacks
+
+# Lambda Layer
+
+- O que é um Lambda Layer?
+  Um Lambda Layer é um pacote ZIP que contém bibliotecas, dependências ou outros arquivos que podem ser usados por funções Lambda. Ele permite que você compartilhe código entre várias funções e mantenha o tamanho do pacote da função menor.
+- Layer e NODEJS_20_X
+  Em casos de uso de NodeJs 20.x, devemos informar o `compatibleRuntimes: [lambda.Runtime.NODEJS_20_X]` no construtor do Lambda Layer. Isso garante que o layer seja compatível com a versão do Node.js que estamos usando.
--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -1,13 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFetchHandler: lambdaNodejs.NodejsFunction; // Declara uma propriedade para a função Lambda
+  readonly productsDdb: dynamodb.Table; // Declara uma propriedade para a tabela DynamoDB
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props); // Chama o construtor da classe base Stack
+
+    // Cria uma tabela DynamoDB com o nome 'ProductsDdb' e uma chave primária 'id'
+    this.productsDdb = new dynamodb.Table(this, 'ProductsDdb', {
+      tableName: 'ProductsDdb', // Nome da tabela
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Política de remoção (destrói a tabela ao excluir a pilha) default: cdk.RemovalPolicy.RETAIN
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING,
+      }, // Chave primária
+      billingMode: dynamodb.BillingMode.PROVISIONED, // Modo de cobrança sob demanda
+      readCapacity: 1, // Capacidade de leitura provisionada, default: 5
+      writeCapacity: 1, // Capacidade de gravação provisionada, default: 5
+    });
 
     // Define a função Lambda Node.js
     this.productsFetchHandler = new lambdaNodejs.NodejsFunction(
@@ -19,12 +34,18 @@ export class ProductsAppStack extends cdk.Stack {
         runtime: lambda.Runtime.NODEJS_20_X, // Versão do Node.js
         handler: 'handler', // Nome do manipulador da função
         memorySize: 512, // Tamanho da memória em MB
-        timeout: cdk.Duration.seconds(5), // Tempo limite de execução da função
+        timeout: cdk.Duration.seconds(10), // Tempo limite de execução da função
         bundling: {
           minify: true, // Minifica o código
           sourceMap: false, // Não gera source maps
         },
+        environment: {
+          PRODUCTS_DDB_TABLE_NAME: this.productsDdb.tableName, // Variável de ambiente com o nome da tabela DynamoDB
+        }, // Variáveis de ambiente para a função Lambda
       }
     );
+
+    // Concede permissão à função Lambda para acessar a tabela DynamoDB
+    this.productsDdb.grantReadData(this.productsFetchHandler); // Permissão de leitura na tabela
   }
 }
